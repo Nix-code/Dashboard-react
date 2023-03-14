58a51922c7c70893d63078a50d2529cdf158f1cc
Nix-code <nishantbanjade07@gmail.com>
2023-03-14 05:52:36 +0530

    update package.json file

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
           <main className="content">
             <Topbar setIsSidebar={setIsSidebar} />
             <Routes>
-              <Route path="/" element={<Dashboard />} />
+              <Route path="/dashboard" element={<Dashboard />} />
               <Route path="/team" element={<Team />} />
                
               <Route path="/contacts" element={<Contacts />} />
